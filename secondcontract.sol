29678e9ca8063d54c05b2de0fe387ca4a47249c1
abuhmhai <abuhmhai@gmail.com>
2024-10-23 01:00:37 +0700

    update

---
+++ secondcontract.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+contract firstcoin {
+    address public minter;
+    mapping (address => uint ) public balances;
+
+    event sent(address from, address to, uint amount);
+    constructor () {
+        minter= msg.sender;
+
+    }
+    function mint(address receiver, uint amount) public{
+        require(msg.sender==minter);
+        require(amount<1e60);
+        balances[receiver]+=amount;
+    }
+    function send(address receiver, uint amount) public{
+        require(amount<=balances[msg.sender],"Khong du tien ma doi chuyen");
+        balances[msg.sender]-=amount;
+        balances[receiver]+=amount;
+        emit sent(msg.sender,receiver, amount);
+    }
+}
