55ed14a3cdb4c643acd246656c379ac7cd06d3ff
abuhmhai <abuhmhai@gmail.com>
2024-10-23 16:40:58 +0700

    data types and address

---
+++ thirddatatype.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.2 <0.9.0;
+contract datatypes{
+    bool myBoolean= true;
+    uint myUnsighnedInteger=10;
+    int myInteger=-10;
+    string myString="my string hello";
+    address myAdress=msg.sender;
+    
+}
+contract Game{
+    uint public countPlayer=0;
+//    Player[] public players;
+    mapping (address=> Player) public players;
+    struct Player{
+        address addressPlayer;
+        string fullName;
+        uint age;
+        string sex;
+    }
+
+    function addPlayer(string memory fullName,uint age, string memory sex) public{
+        players[msg.sender] = Player(msg.sender,fullName, age, sex );
+//        players.push(Player(fullName,age,sex));
+        countPlayer+=1;
+    }
+}
